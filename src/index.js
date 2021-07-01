14d038111c750f661e9ec7ed93df4e46f9e44044
Mike-Max <scartman1992@gmail.com>
2021-07-01 20:02:13 +0100

    search form

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
 import './sass/main.scss';
 import countrys from './tamplates/country-cards.hbs'
+import API from './js/api'
 
 const refs = {
-    countryCards : document.querySelector('.country')
+    countryCards: document.querySelector('.country'),
+    searchForm: document.querySelector('.search-form')
+}
+refs.searchForm.addEventListener('input', onSearch,500)
+
+function onSearch(e) {
+    e.preventDefault();
+
+    const form = e.currentTarget;
+    const searchQuery = form.elements.query.value
+
+API.fetchCountryByName(searchQuery)
+    .then(renderCountry)
+    .catch(error => console.log(error))
+    .finally(() => form.reset())
 }
 
-fetch('https://restcountries.eu/rest/v2/name/portugal')
-    .then(response => response.json())
-    .then(country => {
+
+
+
+
+
+function renderCountry(country) {
         const markup = countrys(country)
         refs.countryCards.innerHTML = markup
-        
     }
-        
-       )
-    .catch(error => console.log(error))
---
+++ src/js/api.js
@@ -0,0 +1,8 @@
+const BASE_URL = 'https://restcountries.eu/rest/v2'
+function fetchCountryByName(name) {
+     
+return  fetch(`${BASE_URL}/name/${name}`)
+    .then(response => response.json())
+}
+
+export default {fetchCountryByName}
